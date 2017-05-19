6cc8660145474df80bf2c695b413a8f50d55cf65
Alenka_Dav <alenkadav239@gmail.com>
2017-05-20 01:18:56 +0300

    оно рисует однотонный фон - осталось разобраться с этим

--- InstantTemperature.js
+++ InstantTemperature.js
@@ -4,10 +4,10 @@ $.InstantTemperature = function (n,time,Length,T1,T2,direction,detalisation,l,w)
 	var r = new Vector;
 	//alert(h+' '+n);
 	//r = direction;
-	for (var i=0; i<n; i++){
+	for (var i=0; i<n+1; i++){
 		T[i] = [];
 		r.x = (i*h - (Length/2)); 
-		for (var j=0; j<n; j++){
+		for (var j=0; j<n+1; j++){
 			r.y = (j*h - (Length/2)); 
 			T[i][j] = $.Integrate(r,time,T1,T2,direction,detalisation,l,w);//
 			//T[i][j].y = r.y;
